0436b6f576048b0f11844c8ad8965531d0c48c82
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-17 01:49:18 -0800

    Stand error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the C
+ *
+ * Return: Returns to the function the value passed
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+		       - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
