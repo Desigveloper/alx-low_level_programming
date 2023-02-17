75ef33a2c1e1294025b332b69cb640d4303b4eed
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-17 04:42:13 -0800

    Print standard to error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char t[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+	char t[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fprintf(stderr, "%s\n", t);
 	return (1);
