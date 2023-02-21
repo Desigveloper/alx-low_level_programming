cdd82ad6aca1f14b0eb7da21cb0392b65c05d02b
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-20 23:51:18 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+	{
+		printf(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
