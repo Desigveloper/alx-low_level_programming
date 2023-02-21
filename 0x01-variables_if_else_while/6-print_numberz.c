cec4025f9e88bf94d5f4f775ad5a73a8cacaa8f5
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-21 00:14:58 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main = The entry point
+ *
+ * Return : Always returnd 0
+ */
+int main(void)
+{
+	char num;
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+	return 0;
+}
