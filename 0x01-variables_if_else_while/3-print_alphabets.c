4d2089322ded615a610eabf181acbdf14507194a
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-20 20:57:35 -0800

    Mixed case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of program
+ *
+ * Return: Always returns 0
+ */
+ 
+ int main(void)
+{
+	char chlow, chcap;
+
+	for(chlow = 'a'; chlow <- 'z'; chlow++)
+	{
+		putchar(chlow);
+	}
+
+	for(for chcap = 'A'; chcap <= 'Z'; chcap++)
+	{
+		putchar(chcap);
+	}
+
+	putchat('\n');
+	return (0);
+}
