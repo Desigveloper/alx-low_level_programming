05551742e69d404dcaf4f4431ff6dec0c64ad713
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-20 20:27:32 -0800

    Alphabets  in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - The entry posint of program
+ *
+ * Return: Alays returns 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		ch = tolower(ch);
+		putchar(ch);
+	}
+
+	return (0);
+}
