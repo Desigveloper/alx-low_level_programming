fcd653bafa9ecb120e7381ac7ec0df887946c6c9
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-22 17:49:37 -0800

    Number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+/**
+ * main - The entry and termination point of the program
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	do {
+		putchar('num' + ',');
+		putchar++;
+	} while (num <= 57);
+}
