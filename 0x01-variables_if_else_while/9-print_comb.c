e56d00cc66811a8281dde62b5aef0140a80e1590
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-22 18:15:28 -0800

    Combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@ int main(void)
 	int  num;
 	num = 48;
 
-	do {
+	while(num <= 57) {
 		putchar(num);
 		putchar(44); /* ASCII code for comma */
 		putchar(32);
 		num++;
-	} while (num <= 57);
+	}
 
 	putchar(10); /* ASCII code for new line */
 	return (0);
