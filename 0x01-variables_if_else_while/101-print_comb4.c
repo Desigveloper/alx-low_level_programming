015896ca52c994f0dbbd48e2d7d83a80ea2d8633
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-24 03:40:46 -0800

    Three number pairing

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+/**
+* main - The entry point.
+*
+* This program prints the possible combination of three digits.
+*
+* Return: Always returns 0 (Success)
+*/
+
+int main(void)
+{
+	int ch1, ch2, ch3;
+
+	for (ch1 = 48; ch2 <= 55; ch1++)
+	{
+		for (ch2 = 48; ch2 <= 56; ch2++)
+		{
+			for (ch3 = 48; ch3 <= 57; ch3++)
+			{
+				if (ch1 == ch2 || ch2 == ch3 || ch1 == ch3)
+				{
+					continue;
+				} else
+				{
+					if (ch1 < ch2 && ch2 < ch3)
+					{
+						putchar(ch1);
+						putchar(ch2);
+						putchar(ch3);
+
+						if (ch1 != 55 || ch2 != 56)
+						{
+							if (ch3 != 57)
+							{
+								putchar(44);
+								putchar(32);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10); /* ASCII code for new line*/
+	return (0);
+}
