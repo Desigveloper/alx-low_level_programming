3101485e91668d72ebfda6b4e336ef070fe5a11b
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-24 04:32:58 -0800

    Pairing three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,26 +17,21 @@ for (ch1 = 48; ch2 <= 55; ch1++)
 	{
 		for (ch3 = 48; ch3 <= 57; ch3++)
 		{
-			if (ch1 == ch2 || ch2 == ch3 || ch1 == ch3)
+			if (ch1 < ch2 && ch2 < ch3)
 			{
-				continue;
-			} else
-			{
-				if (ch1 < ch2 && ch2 < ch3)
+				putchar(ch1);
+				putchar(ch2);
+				putchar(ch3);
+				if (ch1 != 55 || ch2 != 56 || ch3 != 57)
 				{
-					putchar(ch1);
-					putchar(ch2);
-					putchar(ch3);
-					if (ch1 != 55 || ch2 != 56 || ch3 != 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
 }
+
 putchar(10); /* ASCII code for new line*/
 return (0);
 }
