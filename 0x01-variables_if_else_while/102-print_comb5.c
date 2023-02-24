22021d360a5ba167fee1ea1f9829bb3c661f2bc1
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-24 07:19:07 -0800

    possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+/**
+* main - The entry and termination point of the program
+*
+* This program prits the possible combinations of two two-digit numbers.
+*
+* Return: Always return 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	int ch1, ch2;
+
+	for (ch1 = 48; ch1 <= 57; ch1++)
+	{
+		for (ch2 = 48; ch2 <= 57; ch2++)
+		{
+			putchar(ch1);
+			putchar(ch2);
+
+			if (ch1 != 57 && ch2 != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar(10); /* ASCII code for new line*/
+	return (0);
+}
