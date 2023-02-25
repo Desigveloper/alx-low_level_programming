d4b01adc1b62e0255e84f72287b30b16d9c2e569
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-24 19:46:24 -0800

    Comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,17 +11,20 @@ int main(void)
 	int ch1, ch2;
 
 	for (ch1 = 0; ch1 <= 99; ch1++)
-		for (ch2 = 0; ch2 <= 2; ch2++)
+		for (ch2 = 0; ch2 <= 99; ch2++)
 		{
-		putchar((ch1 / 10) + 48);
-		putchar((ch1 % 10) + 48);
-		putchar(32); /*ASCII for space*/
+			putchar((ch1 / 10) + 48);
+			putchar((ch1 % 10) + 48);
+			putchar(32); /*ASCII for space*/
 
-		putchar(ch2 / 10);
-		putchar(ch2 % 10);
-
-		putchar(44); /*ASCII for comma*/
-		putchar(32); /* ASCII for space*/
+			putchar(ch2 / 10);
+			putchar(ch2 % 10);
+		
+			if (ch1 != 98 || ch2 != 99)
+			{
+				putchar(44); /*ASCII for comma*/
+				putchar(32); /* ASCII for space*/
+			}
 		}
 
 
