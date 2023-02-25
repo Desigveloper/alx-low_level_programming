3d4ea9080728087f761970f7a2a7cffb4c5bdc7f
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-24 21:04:02 -0800

    Two-two pair

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,24 @@ int main(void)
 	for (ch1 = 0; ch1 <= 99; ch1++)
 		for (ch2 = 0; ch2 <= 99; ch2++)
 		{
-			putchar((ch1 / 10) + 48);
-			putchar((ch1 % 10) + 48);
-			putchar(32); /*ASCII for space*/
-
-			putchar((ch2 / 10) + 48);
-			putchar((ch2 % 10) + 48);
-
-			if (ch1 != 98 || ch2 != 99)
+			if (ch2 > ch1)
 			{
-				putchar(44); /*ASCII for comma*/
-				putchar(32); /* ASCII for space*/
+				putchar((ch1 / 10) + 48);
+				putchar((ch1 % 10) + 48);
+				putchar(32); /*ASCII for space*/
+
+				putchar((ch2 / 10) + 48);
+				putchar((ch2 % 10) + 48);
+
+				if (ch1 != 98 || ch2 != 99)
+				{
+					putchar(44); /*ASCII for comma*/
+					putchar(32); /* ASCII for space*/
+			
+				}
+		
 			}
 		}
-
 
 	putchar(10); /* ASCII code for new line*/
 	return (0);
