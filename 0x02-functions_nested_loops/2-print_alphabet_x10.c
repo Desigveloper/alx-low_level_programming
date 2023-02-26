0d8a23ff970706cec5857b82ea996d6f4dc30bfc
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-26 03:21:30 -0800

    Alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints lowercase alphabets a-z on 10 line/
+*/
+
+void print_alphabet_x10(void)
+{
+	int count, ch;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar(10);
+}
