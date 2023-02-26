e5b70da119ee215c985f0880ba2e23c554710ff0
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-26 02:49:43 -0800

    aphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entey point and termination point of the program
  * print_alphabet - Prints all lower case alphabets to z
- *Return: Always returns 0 (Succes)
 */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 
 void print_alphabet(void)
 {
