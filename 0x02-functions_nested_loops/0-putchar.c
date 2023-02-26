4b4764a442e0ce2ff28a9a4be3779da9b9784ec7
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-25 17:18:00 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,19 @@
 *
 *  Return: Always returns 0 (Success)
 */
+#include <main.h>
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
