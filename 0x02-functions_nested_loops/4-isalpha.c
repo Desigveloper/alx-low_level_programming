79f094ede5e92fae619a82a4e754865becdb177d
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-26 04:25:19 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Checks if char is a alphabet. Return 1 if true else 0
+ * _isalpha - Checks if char is a alphabet. Return 1 if true else 0
  * @c: argument- takes an int value and pass to the function at execution
  * Return: Returns 1 (Success)
 */
