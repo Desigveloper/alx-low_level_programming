dc86cee455995770386c76695e5b665dca56f198
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-27 12:29:33 -0800

    Time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * @void: returns nothing
+*/
+
+void jac_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			if (h < 10 &&  m < 10)
+			{
+				_putchar('0');
+				_putchar('0' + h);
+				_putchar(':');
+				_putchar('0');
+				_putchar('0' + m);
+			} else if (h < 10 && m > 10)
+			{
+				_putchar('0');
+				_putchar('0' + h);
+				_putchar(':');
+				_putchar('0' + m);
+			} else if (h > 10 && m < 10)
+			{
+				_putchar('0' + h);
+				_putchar(':');
+				_putcahr('0');
+				_putchar('0' + m);
+			} else
+			{
+				_putchar('0' + h);
+				_putchar(':');
+				_putchar('0' + m);
+			}
+		}
+	}
+}
