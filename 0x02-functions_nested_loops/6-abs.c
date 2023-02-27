a6dff9ea542fa03d482adf1375017111e6e282dc
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-26 23:52:36 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _abs - Prints the absolute value of a number(int)
- * @c: Parameter- takes an int argument and pass to the function at execution
+* _abs - Prints the absolute value of a number(int)
+* @c: Parameter- takes an int argument and pass to the function at execution
+* Return: Always returns 0 (Success)
 */
 
 int _abs(int c)
 {
 	_putchar(c);
+	return (0);
 }
