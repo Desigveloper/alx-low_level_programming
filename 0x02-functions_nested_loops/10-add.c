13083f991206644a0856d41c5f7cf239c033d183
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-28 05:17:06 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - takes two integers, sum the and return the value
+ * @n1: first int parameter
+ * @n2: second int parameter
+ * Returns: Returns result
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
