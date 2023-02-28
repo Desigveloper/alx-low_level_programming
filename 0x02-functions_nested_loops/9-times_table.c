de8ce0f4840dfdb9a3a67de91ff4bc5066c4acd9
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-27 23:21:44 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,28 @@
 
 void times_table(void)
 {
-	int row, col;
+	int row, col,  c1, c2, n; 
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 9; col++)
 		{
-			_putchar('0' + (row * col));
+			n = row * col;
+			if (n > 10)
+			{
+				c1 = (row * col) / 10;
+				c2 = (row * col) % 10;
+
+				_putchar('0' + c1);
+				_putchar('0' + c2);
+				_putchar(',');
+				-putchar(' ');
+			} else
+			{
+			_putchar('0' + n);
+			_putchar(',');
+			_putchar(' ');
+			}
 		}
 
 		_putchar('\n');
