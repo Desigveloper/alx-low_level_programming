127426944b68a2b82f4df7949418ed602b8547aa
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-28 13:58:06 -0800

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,15 @@ void print_to_98(int n)
 
 	while(n != cap_num)
 	{
-		if(n > cap)
+		if (n > cap_num)
 		{
-			printf(n);
+			printf("%d", n);
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		} else
 		{
-			printf(n);
+			printf("%d", n);
 			_putchar(',');
 			_putchar(' ');
 			n++;
