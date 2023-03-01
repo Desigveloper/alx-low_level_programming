c82dbb67911dcbea052e573d4d5afcabb1f06c99
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-01 14:25:56 -0800

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,18 @@
 #include  "main.h"
 
 /**
+ * main - The entry point of program
+ *
  * sum_of_all_multiples - Print the sum of all multiples of
  *	3 or 5 below 1024
+ * Return: Always returns zero;
  */
+
+int main(void)
+{
+	sum_of_all_multiples();
+	return (0);
+}
 
 void sum_of_all_multiples(void)
 {
