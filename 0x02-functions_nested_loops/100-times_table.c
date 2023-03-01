cc412ac7a95dd82a721a99084c0c6b9e2f469b28
Habibu Mamudu <Habibumamudu@gmail.com>
2023-02-28 23:37:19 -0800

    multiplication

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the multipliction table of a given a number
+ * @n: parameter
+*/
+
+void print_times_table(int n)
+{
+	int row, col,  c1, c2, product;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			if (n >= 0 && n <= 15)
+			{
+				product = row * col;
+				if (product >= 10)
+				{
+					c1 = (product) / 10;
+					c2 = (product) % 10;
+
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + c1);
+					_putchar('0' + c2);
+
+				} else
+				{
+					if (col != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+
+					_putchar('0' + product);
+				}
+			}
+		}
+
+		_putchar('\n');
+
+	}
+}
