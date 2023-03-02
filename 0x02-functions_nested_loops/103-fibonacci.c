aaa90c08f1decd2be2372b9304fc53354238cfbe
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-02 00:16:15 -0800

    fibonacci 4000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always returns 0
+*/
+void fibonacci(vp)
+
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
+
+/** 
+ * finbonacci - prints fibonacci numbers less than 4000000
+*/
+
+void fibonacci(void)
+{
+	int i = 0;
+	long x = 1, y = 2, sum = y;
+
+	while (x + y < 4000000)
+	{
+		y += x;
+
+		if (y % 2 == 0)
+			sum += y;
+
+		x = y - x;
+
+		++i;
+	}
+
+	printf("%ld\n", sum);
+}
