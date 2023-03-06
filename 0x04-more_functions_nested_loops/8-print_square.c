1adefd69b19c417c80b731bff0394885752079c0
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-06 14:14:10 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print a square of symbols
+ * @size: parameter, input value
+*/
+
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+	}
+}
