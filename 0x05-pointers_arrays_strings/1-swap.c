b61ddef5d21c3a4ee1f685356791d2757cf5d177
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-19 04:59:14 -0700

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - takes two integers and swap them
+ * @a: pointer para 1
+ * @pointer pare 2
+ * returns: Returns nothing
+ */
+
+#include "main.h"
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *b;
+	*b = a;
+	*a = c;
+}
