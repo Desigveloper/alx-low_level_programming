4e5a2d8b3a7f14a320ac9b13217157bb823166aa
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-19 03:58:38 -0700

    derefencing a pointed value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - Takes a pointer parameter and changes value it points to
+ * @n: pointer parameter
+ * return: Returns nothing
+ */
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
