59951f2bbb9d38f96416fcfcd472570ec1acfdd7
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-19 06:02:44 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - prints the lenght of a giving string
+ * @s: String param
+ * return: Return the length of the string
+ */
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
