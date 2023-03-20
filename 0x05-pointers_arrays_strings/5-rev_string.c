b727a4c27b8ebaf3132cb4b6f9049e18dcde09cf
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-20 16:10:36 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+ * rev_strings - print the reverse of a string
+ * @s: param
+ */
+
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	while (i > 0)
+	{
+		i--;
+
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
