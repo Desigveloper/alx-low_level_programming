e93bf3e8c10c9b306b119e6aef4c5d9e5427f741
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-20 15:54:13 -0700

    reversed string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 	}
 
-	for (; i == 0; i--)
+	for (; i > 0; i--)
 	{
 		_putchar(s[i]);
 	}
