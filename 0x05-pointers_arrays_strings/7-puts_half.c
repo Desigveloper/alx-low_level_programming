ead1919be7b963785d29d12db3ea513f7e8009dd
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-21 21:28:11 -0700

    other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+/**
+ * puts_half - splits the string into two parts and print the last half
+ * @str: param
+ */
+
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i, j, k, mid, len = 0;
+	char righthand;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len = i;
+	}
+
+	mid = len / 2;
+
+	for (i = mid, j = 0; i <= len; i++; j++)
+	{
+		righthand = str[i];
+	}
+
+	for (k = 0; k righthand[k] != '\0'; k++)
+	{
+		_putchar(righthand[k]);
+	}
+
+	_putchar('\n');
+
+
+}
