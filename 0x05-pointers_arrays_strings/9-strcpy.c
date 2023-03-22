0223611b5be32d7a7bc877de4edf9369cd28f51b
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-22 00:18:01 -0700

    copy pointed string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+/**
+ * *_dtrcpy - copies the string pointed to one pointer to other
+ * @dest: recrive param
+ * @src: source param
+ */
+
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+
+	return dest;
+}
