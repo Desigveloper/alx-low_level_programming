da6bcf97457808c945cba64a1d603a15f7c0de76
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-22 15:59:38 -0700

    string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,24 +7,21 @@
 
 int _atoi(char *s)
 {
-	int i = 0, num = 0;
+	int i = 0, num = 0, sign = 1;
 
 	for (; s[i] != '\0'; i++)
+
 	{
-		if (s[i] >= '0' && s[i] <= '9' || s[i] == '-')
+		if (s[i] == '-')
+			sign *= -1;
+
+		for (; s[i] >= '0' && s[i] <= '9'; i++)
 		{
 			num = num * 10 + (s[i] - '0');
-		}
-		else
-		{
-			continue;;
 		}
 
 	}
 
-	if (num == NULL)
-		return (0);
-	else
-		return (num);
-
+	num *= sign;
+	return (num);
 }
