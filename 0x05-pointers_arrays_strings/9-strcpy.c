43a39d2c15ccf9494e7444d067c23fbf87b07255
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-22 00:32:59 -0700

    copy pointed string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0, j = 0; src[i] != '\0', (j < i && src[j] != '\0'); i++, j++)
+	for (i = 0; src[i] != '\0'; i++)
+	{}
+
+
+	for (j = 0; (j < i && src[j] != '\0'); j++)
 	{
 		dest[j] = src[j];
 	}
