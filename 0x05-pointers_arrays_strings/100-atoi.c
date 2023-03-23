a860c854ca8072d1a3d3bd8a251861f09ae1fbd0
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-22 19:07:28 -0700

    string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,11 +13,12 @@ int _atoi(char *s)
 	    isi = 0;
 
 
-	while(s[i])
-
+	while (s[i])
 	{
-		if (s[i] == '-')
+		if (s[i] == 45)
+		{
 			min *= -1;
+		}
 
 		for (i = i; s[i] >= '0' && s[i] <= '9'; i++)
 		{
