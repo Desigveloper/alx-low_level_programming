f4eb2ae03c34dd9bfad80bd25c06873efd6426b5
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-25 14:40:20 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++
 	}
 
 	return (dest);
