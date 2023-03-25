39c09031a4dcd0134bfaeb0bd6aac4ca61a3d743
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-25 07:34:42 -0700

    part stru

--- 0x06-pointers_arrays_strings/strncat.c
+++ 0x06-pointers_arrays_strings/strncat.c
@@ -1,1 +1,23 @@
-#include 
+/**
+ * _strncat - concatenates all or a part of src to dest
+ * @dest: string destination param
+ * @src: string src param
+ * @n: index/substring param
+ * Return: Concatenated value in dest
+ */
+
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0,
+	    i;
+
+	while (dest[dlen] != '\0')
+		dlen++;
+
+	for (i = 0; i <= src[n], i++)
+		dest[dlen + 1] = src[i];
+
+	return (dest);
+}
