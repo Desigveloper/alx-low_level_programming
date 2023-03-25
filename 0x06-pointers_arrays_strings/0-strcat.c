816480f4f5736853efde4ee3d294271fc1a8f08c
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-25 07:02:54 -0700

    concat string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,14 @@ char *_strcat(char *dest, char *src)
 	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
 		dlen++;
+	}
 
 	for (i = 0; src[i] != '\0'; i++)
+	{
 		slen++;
-
+	}
 
 	for (i = 0; i <= slen; i++)
 	{
