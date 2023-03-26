98a261eed1b409a01dbf31ca98a44e07dbc8b6b0
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-26 03:47:29 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,27 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 20)
-			str[i + 1] = str[i] - 32;
+		if (i == 0)
+		{
+			if (str[i] >= 97 && str[i] == 122)
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i] == ' ')
+		{
+			++i;
+			if (str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 65 && str[i] <= 90)
+				str[i] = str[i] + 32;
+		}
 	}
 	return (str);
 }
+
