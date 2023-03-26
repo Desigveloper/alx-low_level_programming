1dbc1fc1effb924337cfe11704cf7641c439b258
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-26 05:21:51 -0700

    capitalize word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,30 +9,29 @@
 char *cap_string(char *str)
 {
 	int i;
+	int j;
+	char ch[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0)
 		{
-			if (str[i] >= 97 && str[i] == 122)
-				str[i] = str[i] - 32;
-			continue;
-		}
-		if (str[i] == ' ')
-		{
-			++i;
 			if (str[i] >= 97 && str[i] <= 122)
 			{
 				str[i] = str[i] - 32;
-				continue;
 			}
 		}
-		else
+
+		for  (j = 0; ch[j] != '\0'; j++)
 		{
-			if (str[i] >= 65 && str[i] <= 90)
-				str[i] = str[i] + 32;
+			if (ch[j] == str[i])
+			{
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] = str[i + 1] - 32;
+			}
 		}
 	}
 	return (str);
 }
-
