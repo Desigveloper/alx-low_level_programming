f704c30ef41027d8085447742b825498e1eb51f8
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-26 02:20:44 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 90)
 			str[i] = str[i] - 32;
 	}
 
-	return str[i];
+	return (str);
 }
