16453d2449ec401d30629f2d1ddb637459e10566
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-26 07:45:42 -0700

    rot13 ceaser cipher

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+/**
+ * rot13 - rotates(shift) each char 13 places using ROT13 Cipher(Ceaser)
+ * @str: string to rotate
+ * Return: rotated string
+ */
+
+#include "main.h"
+
+char *rot13(char *str)
+{
+	int i = j = 0;
+	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (; j < 52; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
