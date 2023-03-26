0681bda8240275a3dabea9a9f8d725446df01a8d
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-25 23:52:10 -0700

    compare str

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int ch;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (ch = 0; s1[ch] != '\0' || s2[ch] != '\0'; ch++)
 	{
-			return (s1[i] - s2[i]);
+			return (s1[ch] - s2[ch]);
 	}
 }
