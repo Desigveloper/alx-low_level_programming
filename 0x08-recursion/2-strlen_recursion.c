7f3a3900c0c93ce5d6dd9318782bba8f8a2d7426
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-27 09:39:13 -0700

    len of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: strig to be counted
+ * Returns: length of string
+ */
+
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+	int strlen = 0;
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_strlen_recursion(s + 1);
+		strlen++;
+	}
+	return (strlen);
+}
