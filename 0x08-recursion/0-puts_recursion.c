327cf882b591cbc5bfe1e7794c42667469233e43
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-27 05:23:24 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * puts_recursion - prints a string
+ * @s: string to be recursed
+ * Return: nothing
+ */
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '\0')
+			return;
+	}
+
+	_puts_recursion(s[i] + 1);
+}
