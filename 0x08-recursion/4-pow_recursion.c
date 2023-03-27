35fffbca60d0d1041a5a125561f89a01f07b637e
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-27 10:18:22 -0700

    power recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,13 +9,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	int result;
-
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return 1;
 
-	return (x * pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
  
 }
