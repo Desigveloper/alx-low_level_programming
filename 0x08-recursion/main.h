783426fb5e8bcff1b76bfed58116740271593250
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-28 01:20:47 -0700

    is palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int find_root(int n, int x);
+int find_root(int n, int x); /* Helper function sqrt */
 int is_prime_number(int n);
 int is_palindrome(char *s);
+char rev_str(char); /* Helper function palindrome */
+
 #endif /* MAIN_H */
