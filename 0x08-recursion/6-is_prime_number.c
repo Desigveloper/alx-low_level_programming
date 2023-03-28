e441f3253bd90213637e7471e588a0a15566ac61
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-28 00:45:21 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,12 +7,12 @@
 #include "main.h"
 
 int is_prime_number(int n)
-{
-	is_prime_number(n + 1);
 
 	if (!(n % 2 == 0 || n % 3 == 0 || n % 5 == 0))
 		if (!(n <= 1))
 			return (1);
 	
 	return (0);
+
+	is_prime_number(n + 1);
 }
