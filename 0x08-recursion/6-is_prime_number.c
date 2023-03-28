be2c9870d9d307904a5adf869fdd0e39a447f6a0
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-28 00:35:39 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,9 +8,9 @@
 
 int is_prime_number(int n)
 {
-	if !(n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	if (!(n % 2 == 0 || n % 3 == 0 || n % 5 == 0))
 	{
-		if !(n <= 1)
+		if (!(n <= 1))
 			return (1);
 	}
 	else
