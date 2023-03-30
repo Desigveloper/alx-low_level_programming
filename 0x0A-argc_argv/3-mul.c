8d76bb361790795a37b3b41d28934fe2f0c05bc7
Habibu Mamudu <Habibumamudu@gmail.com>
2023-03-30 03:52:52 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer string array
+ */
+
+#include "main.h"
+
+int main(int argc, char argv[])
+{
+	int n1, n2;
+	long product;
+
+	if (argc != 3)
+	{
+		puts("Error");
+	}
+	else
+	{
+		product = argv[1] * argv[2];
+		printf("%ld\n", product);
+	}
+
+	return (0);
+}
