594413b1f4da7c46dd43e1b2530da1981577a244
Habibu Mamudu <Habibumamudu@gmail.com>
2023-04-01 13:44:13 -0700

    split string to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,91 @@
+#include "main.h"
+/**
+ * strtow -  splits a string into words
+ *
+ * @str: string to splear
+ *
+ * Return:  pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	int i, j, k, l, n1 = 0, qa = 0;
+	char **q;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	n1 = words_count(str);
+
+	if (n1 == 1)
+		return (NULL);
+
+	q = (char **)malloc(n1 * sizeof(char *));
+
+	if (q == NULL)
+		return (NULL);
+	
+	q[n1 - 1] = NULL;
+	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+				j++;
+			
+			q[qa] = (char *)malloc(j * sizeof(char));
+			j--;
+
+			if (q[qa] == NULL)
+			{
+				for (k = 0; k < qa; k++)
+					free(q[k]);
+				free(q[n1 - 1]);
+					free(q);
+
+				return (NULL);
+			}
+
+			for (l = 0; l < j; l++)
+				q[qa][l] = str[i + l];
+
+			q[qa][l] = '\0';
+				qa++;
+				i += j;
+		}
+		else
+			i++;
+	}
+
+	return (q);
+}
+
+/**
+ * wordscount - counts the number of words in a string
+ * @s: string to count
+ *
+ * Return: int of number of words
+ */
+
+int words_count(char *str)
+{
+	int i = 0, n2 = 0;
+
+	while (str[i])
+	{
+		if (str[i] == ' ')
+		{
+			if (str[i + 1] != ' ' && str[i + 1] != '\0')
+				n2++;
+		}
+		else if (i == 0)
+			n2++;
+
+		n2++;
+	}
+
+	n2++;
+	return (n2);
+}
