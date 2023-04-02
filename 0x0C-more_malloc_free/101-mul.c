4b8cb476b816af8faffc70e939a89f163d609509
Habibu Mamudu <Habibumamudu@gmail.com>
2023-04-02 02:58:09 -0700

    mutiply numbers with mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -62,28 +62,30 @@ int main(int argc, char *argv[])
  */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-        int j, k, mul, mulrem, add, addrem;
+	int j, k, mul, mulrem, add, addrem;
 
-        mulrem = addrem = 0;
-        for (j = num_index, k = dest_index; j >= 0; j--, k--)
-        {
-                mul = (n - '0') * (num[j] - '0') + mulrem;
-                mulrem = mul / 10;
-                add = (dest[k] - '0') + (mul % 10) + addrem;
-                addrem = add / 10;
-                dest[k] = add % 10 + '0';
-        }
-        for (addrem += mulrem; k >= 0 && addrem; k--)
-        {
-                add = (dest[k] - '0') + addrem;
-                addrem = add / 10;
-                dest[k] = add % 10 + '0';
-        }
-        if (addrem)
-        {
-                return (NULL);
-        }
-        return (dest);
+	mulrem = addrem = 0;
+
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
 }
 
 /**
@@ -95,11 +97,11 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
  */
 void init(char *str, int l)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < l; i++)
-                str[i] = '0';
-        str[i] = '\0';
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
 }
 
 /**
@@ -110,17 +112,17 @@ void init(char *str, int l)
  */
 int check_for_digits(char **av)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 1; i < 3; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                {
-                        if (av[i][j] < '0' || av[i][j] > '9')
-                                return (1);
-                }
-        }
-        return (0);
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
 }
 
 /**
@@ -132,18 +134,17 @@ int check_for_digits(char **av)
  */
 void _print(char *str, int l)
 {
-        int i, j;
+	int i, j = 0;
 
-        i = j = 0;
-        while (i < l)
-        {
-                if (str[i] != '0')
-                        j = 1;
-                if (j || i == l - 1)
-                        _putchar(str[i]);
-                i++;
-        }
+	for (i = 0; i < l; i++)
+	{
+		if (str[i] != '0')
+			j = 1;
 
-        _putchar('\n');
-        free(str);
+		if (j || i == l - 1)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+	free(str);
 }
