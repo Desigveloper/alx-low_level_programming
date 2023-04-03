b1bfd30a36c2c48484afa2424015c2941f561839
Habibu M <habibumamudu@gmail.com>
2023-04-03 04:35:57 -0700

    macro with token

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef 0-OBJECT_LIKE_MACRO_H
+#define 0-OBJECT_LIKE_MACRO_H
+
+/* defines a macro named SIZE 
+ * as an abbreviation for the token 1024. */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+ALX Task 0x0D: Preprocessors
