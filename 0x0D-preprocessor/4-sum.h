a29707903fc318ea4cf111bfa561544f324de167
Habibu M <habibumamudu@gmail.com>
2023-04-03 05:20:57 -0700

    sum nums with SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * SUM(x, y) - a function-like macro
+ * that computes the sum of the numbers x and y.
+ *
+ * @x: param 1
+ * @y: param 2
+ */
+
+#define SUM(x, y)(x + y)
+#endif
