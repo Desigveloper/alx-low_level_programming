87431ba6bc638e894df1ae9ffdcfd854791015cc
Habibu M <habibumamudu@gmail.com>
2023-04-04 02:42:53 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,9 +12,9 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
 	int len_n = _strlen(name);
 	int len_o = _strlen(owner);
-	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
