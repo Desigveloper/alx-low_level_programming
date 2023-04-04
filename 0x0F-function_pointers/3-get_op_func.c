552940bd7bc6e6f4b2977863e76ed182533f3403
Habibu M <habibumamudu@gmail.com>
2023-04-04 05:41:48 -0700

    operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
+	int i = 0;
 
 
 	op_t ops[] = {
@@ -22,10 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 
-	for (i = 0; (ops[i].op); i++)
+	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
