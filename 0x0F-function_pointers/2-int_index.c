84ac17f489a77bd94e1889146205a08354241414
Habibu M <habibumamudu@gmail.com>
2023-04-04 04:49:33 -0700

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array && cmp)
+	if (array != 0 && cmp != 0)
 	{
 		while (i < size)
 		{
-			if (cmp(array[i]) != 0)
+			if (cmp(*(array + i)) != 0)
 				return (i);
 			i++;
 		}
+	}
 
 	return (-1);
 }
