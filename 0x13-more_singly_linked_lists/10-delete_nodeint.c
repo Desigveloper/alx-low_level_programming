1047e42d8e3aabe2bca3cac28d3ae7e9e6d20458
Habibu M <habibumamudu@gmail.com>
2023-04-04 23:53:44 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp = *head;
 	listint_t *current_node = NULL;
-	int i;
+	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
