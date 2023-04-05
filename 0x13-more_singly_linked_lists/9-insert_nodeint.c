d92490d3bd344ccc70961cc1a713d7d0a2e29fea
Habibu M <habibumamudu@gmail.com>
2023-04-04 23:43:18 -0700

    insert node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list
- * 		at a given position
+ *		at a given position
  *
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
