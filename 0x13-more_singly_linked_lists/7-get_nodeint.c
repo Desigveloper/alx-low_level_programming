3595cd261477d3e45398fa0d4d6827a20098340d
Habibu M <habibumamudu@gmail.com>
2023-04-04 23:13:24 -0700

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *tmp = head;
 
-	for ( i = 0; tmp && i < index; i++)
+	for (i = 0; tmp && i < index; i++)
 	{
 		tmp = tmp->next;
 	}
