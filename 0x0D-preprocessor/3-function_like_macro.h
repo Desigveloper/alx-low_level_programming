47c40175789435499ae83343d1cc9d139a54b328
Habibu Mamudu <59403534+habeeb25@users.noreply.github.com>
2023-04-23 10:51:46 +0000

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-/**
- * ABS(x) - computes the absolute value of a number x
- * @x: multiplier
- */
-
-#define ABS(x)((x) < (0) ? -(x) : (x))
-#endif
--- 0x0D-preprocessor/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-#include <stdio.h>:wq
-#include "object_like_macro.h"
-#include "pi"
-
-#endif /* MAIN_H */
