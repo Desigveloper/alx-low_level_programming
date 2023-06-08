e4503f3964d0c59372b02693bc864b9f53b23822
desigveloper <habibumamudu@gmail.com>
2023-06-07 20:40:14 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,34 +20,25 @@ int _sqrt_recursion(int n)
 		return (n);
 	}
 
-	return (sqrt_helper_func(n, 1, n)); /* recursive call */
+	return (sqrt_helper_func(n, 0)); /* recursive call */
 }
 
 
 /**
  * sqrt_helper_func - a recursive helper function that takes three arguments
  * @iNum: param1, an integer
- * @iStart: param2, the starting point
- * @iEnd: param3: the ending point.
+ * @iRoot: param2, root of the number
  *
  * Return: n or the dermined value aftersearch
  */
 
-int sqrt_helper_func(int iNum, int iStart, int iEnd)
+int sqrt_helper_func(int iNum, int iRoot)
 
 {
-	int iMid;
 
-	if (iStart > iEnd)
+	if (iRoot * iRoot == iNum) /* found the sqrt */
 	{
-		return (iEnd);
-	}
-
-	iMid = (iStart + iEnd) / 2;
-
-	if (iMid * iMid == iNum) /* found the sqrt */
-	{
-		return (iMid);
+		return (iRoot);
 	}
 	else if (iMid * iMid > iNum) /* search in the lower half */
 	{
@@ -55,7 +46,7 @@ int sqrt_helper_func(int iNum, int iStart, int iEnd)
 	}
 	else /* search in the upper half */
 	{
-		return (sqrt_helper_func(iNum, iMid + 1, iEnd));
+		return (sqrt_helper_func(iNum, iMid + 1));
 	}
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int sqrt_helper_func(int iNum, int iStart, int iEnd);
+int sqrt_helper_func(int iNum, int iRoot);
 #endif /*MAIN_H*/
