c62c0b8f7fd9d10b45cfd14b694a106554a55ab9
desigveloper <habibumamudu@gmail.com>
2023-06-07 19:28:55 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,16 +9,22 @@
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0) {
-        return -1;
-    }
-    if (n == 0 || n == 1) {
-        return n;
-    }
-    int i = 1, result = 1;
-    while (result <= n) {
-        i++;
-        result = i * i;
-    }
-    return _sqrt_recursion(n - (i - 1)) + (i - 1);
+	int i = 1, iResult = 1;
+
+	if (n < 0)
+	{
+		return -1;
+	}
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+
+
+	for (; iResult <= n; i++)
+	{
+		iResult = i * i;
+	}
+
+	return _sqrt_recursion(n - (i - 1)) + (i - 1);
 }
