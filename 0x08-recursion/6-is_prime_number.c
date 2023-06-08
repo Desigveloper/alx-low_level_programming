2dde693e1dba538a9a6c037735d68ca2e0072b11
desigveloper <habibumamudu@gmail.com>
2023-06-07 23:02:01 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,7 +19,7 @@ int is_prime_helper_func(int iNum, int iCounter)
 	if (iCounter == 1)
 		return (1);
 
-	if (int iNum % iCounter == 0)
+	if (iNum % iCounter == 0)
 		return (0);
 
 	return (is_prime_helper_func(iNum, iNum - 1));
