979fd3d70ad2c64bb997512c46246eb8613aa3a2
desigveloper <habibumamudu@gmail.com>
2023-06-07 19:57:19 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 /**
- * _sqrt_recursion - calculates and returns the natural square root of a number
+ * _sqrt_recursion - Takes an int n and returns its natural sqrt using recursion
  * @n: param,  an integer representing number to be checked
  *
  * Return: value of n , or -1 as an error
@@ -7,24 +7,55 @@
 
 #include "main.h"
 
+
 int _sqrt_recursion(int n)
 {
-	int i = 1, iResult = 1;
-
-	if (n < 0)
+	if (n < 0) /* error case */
 	{
-		return -1;
-	}
-	if (n == 0 || n == 1)
-	{
-		return n;
+		return (-1);
 	}
 
-
-	for (; iResult <= n; i++)
+	if (n == 0 || n == 1) /* base case */
 	{
-		iResult = i * i;
+		return (n);
 	}
 
-	return _sqrt_recursion(n - (i - 1)) + (i - 1);
+	return (sqrt_helper_func(n, 1, n)); /* recursive call */
 }
+
+
+/**
+ * sqrt_helper_func - a recursive helper function that takes three arguments
+ * @iNum: param1, an integer
+ * @iStart: param2, the starting point
+ * @iEnd: param3: the ending point.
+ *
+ * Return: n or the dermined value aftersearch
+ */
+
+int sqrt_helper_func(int iNum, int iStart, int iEnd)
+
+{
+	int iMid;
+
+	if (iStart > iEnd)
+	{
+		return (iEnd);
+	}
+
+	iMid = (iStart + iEnd) / 2;
+
+	if (iMid * iMid == iNum) /* found the sqrt */
+	{
+		return (iMid);
+	}
+	else if (iMid * iMid > iNum) /* search in the lower half */
+	{
+		return (sqrt_helper_func(iNum, iStart, iMid - 1));
+	}
+	else /* search in the upper half */
+	{
+		return (sqrt_helper_func(iNum, iMid + 1, iEnd));
+	}
+}
+
