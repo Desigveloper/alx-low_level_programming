5963fb7b257ed08ec83369d087b630807016d637
desigveloper <habibumamudu@gmail.com>
2023-06-10 10:37:37 -0700

    file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+/**
+ * main - prints the name of the file using command line argument
+ * @argc: argument count
+ * @argv: passed aruguments
+ * Return: 0 (success)
+ */
+
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+        int ch = 0;
+
+        for  (;ch < argc; ch++)
+                printf("%s\n", *(argv + 0));
+
+        return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+0x0A. C - argc, argv
+
+0-whatsmyname.c: A program that prints its name, followed by a new line.
+If the program is renamed, it will print the new name, without having to compile it again.
