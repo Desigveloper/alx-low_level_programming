58951a86ca59e78b6a584a15bb935a3729a82c41
desigveloper <habibumamudu@gmail.com>
2023-06-13 21:31:26 -0700

    string as param

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+/**
+ * _strdup -pointer to a new string which is a duplicate of the string str
+ *
+ * @str: string to copy
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *ptrStr;
+
+	if (str == NULL)
+		return (NULL);
+
+
+	for (i = 0; str[i]; i++)
+		;
+	
+
+	ptrStr = malloc(i * sizeof(char));
+
+	if (ptrStr == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		*(ptrStr + j) = *(str + j);
+
+	return (ptrStr);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)i;
+char *_strdup(char *str);
 #endif
