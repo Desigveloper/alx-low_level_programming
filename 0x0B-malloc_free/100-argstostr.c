901b83375e1adfcf536894eabf73c9178fccbe26
desigveloper <habibumamudu@gmail.com>
2023-06-14 20:36:27 -0700

    concatenates all argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+/**
+ * argstostr - joins all the arguments of the program
+ *
+ * @ac: grid to free the previous
+ * @av: height of grid
+ *
+ * Return: pointer to a new string, or NULL if it fails
+ */
+
+#include "main.h"
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, n = 0;
+
+	char *str;
+
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac ; i++)
+	{
+		for (j = 0; *(*(av + i) + j); j++)
+			n++;
+		n++;
+	}
+
+	n++;
+	str = malloc(n * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j); j++)
+		{
+			*(str + k) = *(*(av + i) + j);
+			k++;
+		}
+		*(str + k) = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+	return (str);
+
+}
