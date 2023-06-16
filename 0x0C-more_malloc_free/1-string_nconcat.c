0cb2ceb023ebf10c5e3abda534d0b5b724fef3dd
desigveloper <habibumamudu@gmail.com>
2023-06-15 23:54:35 -0700

    concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * string_nconcat - concatenates two strings.
  *
@@ -9,17 +7,20 @@
  *
  * Return: NULL if the function fails
  */
+
+#include "main.h"
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1, len2, i;
-	char *p_result;
+	char *ptr_result;
 
 	if (s1 == NULL)
 		len1 = 0;
 	else
 	{
 		for (len1 = 0; *(s1 + len1); ++len1)
-		;
+			;
 	}
 
 	if (s2 == NULL)
@@ -27,23 +28,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		for (len2 = 0; *(s2 + len2); ++len2)
-		;
+			;
 	}
 
 	if (len2 > n)
 		len2 = n;
 
-	p_result = malloc(sizeof(char) * (len1 + len2 + 1));
+	ptr_result = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (p_result == NULL)
+	if (ptr_result == NULL)
 		return (NULL);
 
-	for (i = 0; i < x; i++)
-		s[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		*(ptr_result + i) = *(s1 + i);
 
 	for (i = 0; i < len2; i++)
-		p_result[i + len1] = s2[i];
+		*(ptr_result + (i + len1)) = *(s2 + i);
 
 	p_result[len1 + len2] = '\0';
-	return (p_result);
+	return (ptr_result);
 }
