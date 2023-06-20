617e1ca551f1d2feeee0c30d9a89ab08722b43cf
desigveloper <habibumamudu@gmail.com>
2023-06-20 16:13:22 -0700

    the object like macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef __MY_HEADER_FILE_H__
-#define __MY_HEADER_FILE_H__
+#ifndef 0_OBJECT_LIKE_MACRO_H
+#define 0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* __MY_HEADER_FILE_H__ */
+#endif /* 0_OBJECT_LIKE_MACRO_H */
