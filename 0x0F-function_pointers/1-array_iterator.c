289550ce860ffae013ed9fddc7f23fba7b6c576a
desigveloper <habibumamudu@gmail.com>
2023-06-22 00:56:47 -0700

    iterates array of given elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int;
+
 	if (array == NULL || action == NULL)
 		return;
 
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(*(array + i));
 	}
