1aeda5e18f646780fac53557413da765be9d2f16
desigveloper <habibumamudu@gmail.com>
2023-06-22 00:33:28 -0700

    pointer to function thats prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
 /**
  * print_name - take pointer to cahr atgument and prints to the screen as a name
  * @name: param, pointer to char
- *
+ * @f: pointer to the printing function
  * Return: nothing
  */
 
-#include "main.h"
+#include "function_pointers.h"
+
 void print_name(char *name, void (*f)(char *))
 {
+	if(name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
