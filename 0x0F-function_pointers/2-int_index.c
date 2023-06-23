5359d73a8401b9ffb2c64451c170e176139dba13
desigveloper <habibumamudu@gmail.com>
2023-06-22 21:30:12 -0700

    compare int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == 0 && cmp == 0)
-
-	for (i = 0; i < size; i++)
+	if (array != 0 && cmp != 0)
 	{
-		if (cmp(*(array + i)) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(*(array + i)) != 0)
+				return (i);
+		}
 	}
 
 	return (-1);
