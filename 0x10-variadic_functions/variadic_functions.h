90a5e6b1816c7e045a42314c92ab4f6cf03f606c
desigveloper <habibumamudu@gmail.com>
2023-06-24 01:00:55 -0700

    print all format specifiers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,21 +3,24 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdarg.h> /* for va_list, va_start,and va_end macros */
 #include <unistd.h>
 
-typedef struct data
+/* STRUCTURE OR STRUCT TAG DEFINITION */
+typedef struct print_type
 {
-	char *str;
-	int num;
-	float fnum;
-	char c;
-} p_data;
+	char type; /* formart specifier char */
+	void (*print)(va_list); /* pointer to the func printing corresp arg */
+} inst_of_print_type;
 
+/* FUNCTION PROTOTYPES */
 int _putchar(char ch);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 #endif
