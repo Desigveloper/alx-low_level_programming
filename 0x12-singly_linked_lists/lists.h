3101ccc06e9977ee0d467e3df27820aebf386190
desigveloper <habibumamudu@gmail.com>
2023-07-01 21:32:03 -0700

    function that prints each node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @strLen: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct s_list {
+	char *str;
+	int len;
+	struct s_list *next;
+} list_t;
+
 
 int _putchar(void);
 size_t print_list(const list_t *h);
