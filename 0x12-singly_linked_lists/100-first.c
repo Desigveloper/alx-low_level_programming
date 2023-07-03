5b2b2658f22bdeea56ac17187746a838680ca421
desigveloper <habibumamudu@gmail.com>
2023-07-03 01:00:24 -0700

    print message to the console

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+/**
+ * printMessageBeforeMain -  Prints the message "You're beat! and yet, /
+ * you must allow,I bore my house upon my back!" to the console.
+ *
+ */
+
+#include "lists.h"
+
+void printMessageBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,4 +29,5 @@ list_t *add_node(list_t **head, const char *str);
 size_t _strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void printMessageBeforeMain(void) __attribute__((constructor));
 #endif
