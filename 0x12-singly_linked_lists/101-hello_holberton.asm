ed2312ebddb28a9c910ed18ac33033a72272ceb4
desigveloper <habibumamudu@gmail.com>
2023-07-03 21:46:56 -0700

    print a text with assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+	global	main
+	extern	printf
+main:
+	mov	edi, format
+	xor	eax, eax
+	call	printf ;calls the printf function
+	mov	eax, 0 ;moves the address of the string to a register
+	ret
+;defines a string containt a text
+format: db "Hello, Holberton\n",0
