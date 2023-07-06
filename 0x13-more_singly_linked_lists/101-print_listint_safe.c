3b9368440e8c38c198a3e908a540ce264fd2b92e
desigveloper <habibumamudu@gmail.com>
2023-07-06 00:55:31 -0700

    print address

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,30 +9,23 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp_node1, *temp_node2;
+	const listint_t *temp_node;
 	size_t count = 0;
 
-	if (head == NULL)
-		exit(98);
 
-	temp_node1 = head;
-	temp_node2 = head->next;
-
-	while (temp_node1 != NULL)
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)temp_node1, temp_node1->n);
 		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
 
-		if (temp_node2 != NULL && temp_node2 <= temp_node1)
+		temp_node = head;
+		head = head->next;
+
+		if (temp_node <= head)
 		{
-			printf("-> [%p] %d\n", (void *)temp_node2, temp_node2->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			exit(98);
 		}
-
-		temp_node1 = temp_node1->next;
-
-		if (temp_node2 != NULL)
-			temp_node2 = temp_node2->next;
 	}
 
 	return (count);
