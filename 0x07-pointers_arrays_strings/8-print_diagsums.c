bbdc680b6daecf360e7855de675a3fe095fd281e
desigveloper <habibumamudu@gmail.com>
2023-07-09 03:02:43 -0700

    sum diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
 /**
- * print_diagsum - Calc and prints the sum of the 2 diagonals of a sqr matrix.
+ * print_diagsums - Calc and prints the sum of the 2 diagonals of a sqr matrix.
  * 
  * @a: Pointer to the first element of the matrix
  * @size: Size of the matrix (number of rows/columns)
@@ -15,8 +15,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + (i*size + i));
-		sum2 += *(a + (i*size + (size - i - 1)));
+		sum1 += *(a + (i * size + i));
+		sum2 += *(a + (i * size + (size - i - 1)));
 	}
 
 	printf("%d, %d\n", sum1, sum2);
