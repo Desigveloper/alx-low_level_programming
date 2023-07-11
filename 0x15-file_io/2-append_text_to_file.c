14af09dece10576393ba50b696883a436c0f023d
desigveloper <habibumamudu@gmail.com>
2023-07-11 02:14:11 -0700

    appends a text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 /**
  * append_text_to_file - Appends text at the end of a file.
- * 
+ *
  * @filename: The name of the file.
  * @text_content: The NULL terminated string to add at the end of the file.
- * Return 1 on success and -1 on failure. If filename is NULL, return -1.
+ * Return: 1 on success and -1 on failure. If filename is NULL, return -1.
  */
 
 #include "main.h"
