9a91a8f001bffbb05bbdd2372ad219bd963104be
desigveloper <habibumamudu@gmail.com>
2023-07-19 22:46:11 -0700

    copy content from file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,7 +16,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char* print_err_msg(char *msg);
-void copy_content(int fd_from, int fd_to, ssize_t bytes_read, char *buffer);
 void close_fd (int fd_from,  int fd_to, char *err_msg);
 
 #endif
