67b197dcf2218f8af9602e7b4214cb53891b603e
Habibu Mamudu <habibumamudu@gmail.com>
2023-08-23 17:55:35 +0000

    prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,10 +15,8 @@ size_t print_dlistint(const dlistint_t *h)
 	while (current != NULL)
 	{
 		printf("%d", current->n);
-		
 		count++;
 		current = current->next;
 	}
-	printf("\n");
 	return (count);
 }
