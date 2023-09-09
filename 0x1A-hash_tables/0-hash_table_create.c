2555337db7b627b1763c476ebdf8df0f8e6c3e59
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-09 04:43:33 +0000

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "hash_tables.h"
 
 /**
  * hash_table_create - creates a hash table
@@ -6,13 +6,15 @@
  *
  * Return: pointer to the newly created hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (hash_table == NULL)
+	{
 		return (NULL);
+	}
 
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
@@ -23,8 +25,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
+	{
 		hash_table->array[i] = NULL;
+	}
 
 	return (hash_table);
 }
