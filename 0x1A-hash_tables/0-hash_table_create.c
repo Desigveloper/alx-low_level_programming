f3e257100c1dbf919aea91068ce0444fda255133
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-09 04:21:34 +0000

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,30 @@
 #include <stdlib.h>
 
-typedef struct {
-    unsigned long int size;
-    /* Other fields of the hash table */
-} hash_table_t;
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
 
-hash_table_t *hash_table_create(unsigned long int size) {
-    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-    
-    if (hash_table == NULL) {
-        return NULL; /* Return NULL if memory allocation failed */
-    }
-    
-    hash_table->size = size;
-    
-    /* Initialize other fields of the hash table */
-    
-    return hash_table;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (unsigned long int i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+
+	return (hash_table);
 }
