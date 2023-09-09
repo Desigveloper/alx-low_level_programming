8f3536f235922c1ed873d928f6410dfc4e387561
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-09 15:05:09 +0000

    add element to hash t

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -53,13 +53,14 @@ void update_node_value(hash_node_t *node, const char *value)
 * create_node - Creates a new node with the specified key and value
 * and adds it to the hash table
 * @ht: Pointer to the hash table
-* @k: The key for the new node
-* @val: The value to the new node
-* @ind: The index in the array where the new node should be added
+* @key: The key for the new node
+* @value: The value to the new node
+* @index: The index in the array where the new node should be added
 *
 * Return: 1 if the operation succeeded, 0 otherwise
 */
-int create_node(hash_table_t *ht, const char *k, const char *val, unsigned long int ind)
+int create_node(hash_table_t *ht, const char *key,
+const char *value, unsigned long int index)
 {
 	hash_node_t *new_node;
 	char *key_copy;
@@ -70,10 +71,10 @@ int create_node(hash_table_t *ht, const char *k, const char *val, unsigned long 
 	if (new_node == NULL)
 		return (0); /* Memory allocation failed */
 
-	key_copy = strdup(k);
-	value_copy = (val != NULL) ? strdup(val) : NULL;
+	key_copy = strdup(key);
+	value_copy = (value != NULL) ? strdup(value) : NULL;
 
-	if (key_copy == NULL || (val != NULL && value_copy == NULL))
+	if (key_copy == NULL || (value != NULL && value_copy == NULL))
 	{
 		free(new_node);
 		free(key_copy);
@@ -83,8 +84,8 @@ int create_node(hash_table_t *ht, const char *k, const char *val, unsigned long 
 
 	new_node->key = key_copy;
 	new_node->value = value_copy;
-	new_node->next = ht->array[ind];
-	ht->array[ind] = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1); /* Operation succeeded */
 }
