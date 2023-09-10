4cb46d652bb8e58c1429ceeb30732bae34dd3170
Habibu Mamudu <habibumamudu@gmail.com>
2023-09-10 09:52:07 +0000

    hash table get element >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
 * @ht: Pointer to the hash table to search in
 * @key: The key to look for
 *
-* Returns: The value associated with the key, or NULL if not found
+* Return: The value associated with the key, or NULL if not found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
